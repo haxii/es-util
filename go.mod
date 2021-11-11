0a1ef8d506a7ed93bf312740775586d9803402ac
Zichao LI <cszichao@gmail.com>
2021-11-11 11:41:58 +0800

    basic util impl

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/haxii/es-util
+
+go 1.16
+
+require (
+	github.com/olivere/elastic/v7 v7.0.29
+	github.com/pkg/errors v0.9.1
+	go.uber.org/multierr v1.7.0
+)
---
+++ util.go
@@ -0,0 +1,95 @@
+package es_util
+
+import (
+	"context"
+	"github.com/olivere/elastic/v7"
+	"github.com/pkg/errors"
+	"go.uber.org/multierr"
+	"time"
+)
+
+func StrArrToInterfaceArray(a []string) []interface{} {
+	b := make([]interface{}, len(a))
+	for i := range a {
+		b[i] = a[i]
+	}
+	return b
+}
+
+func Int64ArrToInterfaceArray(a []int64) []interface{} {
+	b := make([]interface{}, len(a))
+	for i := range a {
+		b[i] = a[i]
+	}
+	return b
+}
+
+// MakeBulkESRequest 分批执行 ES 的批量请求，避免 payload 过大造成集群宕机
+//
+// bulkSize 最终插入的总数据量, limit 为每批批量插入的数据量, reqGap 为每批插入的时间间隔,
+// makeSingleRequest 应按数据的序号返回对应的每个批量请求的具体内容,
+// 返回成功的数量
+func MakeBulkESRequest(es *elastic.Client, bulkSize, limit int, reqGap time.Duration,
+	makeSingleRequest func(int) elastic.BulkableRequest) (successCount int, err error) {
+	// 保证传入数据的正确性
+	if es == nil || makeSingleRequest == nil {
+		return 0, errors.New("invalid es client or index")
+	}
+	if bulkSize <= 0 {
+		return 0, nil
+	}
+	if limit <= 0 || limit > bulkSize {
+		limit = bulkSize
+	}
+	// 分割次数
+	round := bulkSize/limit + 1
+	shouldSleep := false
+	for roundIndex := 0; roundIndex < round; roundIndex++ {
+		bulkRequest := es.Bulk()
+		bulkRequestValid := false // 判断是否真的有请求加到里面去了
+		// 每轮执行 bulk request
+		for t := 0; t < limit; t++ {
+			requestIndex := roundIndex*limit + t
+			if requestIndex >= bulkSize {
+				break
+			}
+			req := makeSingleRequest(requestIndex)
+			if req != nil {
+				bulkRequest.Add(req)
+				bulkRequestValid = true
+			}
+		}
+		if !bulkRequestValid {
+			continue
+		}
+		if shouldSleep { // 非第一次批量请求前执行 sleep
+			time.Sleep(reqGap)
+		}
+		resp, bulkReqErr := bulkRequest.Do(context.TODO())
+		shouldSleep = true
+		if bulkReqErr != nil {
+			return 0, bulkReqErr
+		}
+		roundCount, roundErr := ParseBulkResponse(resp)
+		if roundErr != nil {
+			err = multierr.Append(err, roundErr)
+		}
+		successCount += roundCount
+	}
+	return
+}
+
+func ParseBulkResponse(resp *elastic.BulkResponse) (successCount int, err error) {
+	if len(resp.Failed()) > 0 {
+		for _, f := range resp.Failed() {
+			if f.Error == nil {
+				continue
+			}
+			err = multierr.Append(err, errors.Errorf("es error: %s [type=%s]", f.Error.Reason, f.Error.Type))
+		}
+		successCount = len(resp.Items) - len(resp.Failed())
+	} else {
+		successCount = len(resp.Succeeded())
+	}
+	return
+}
