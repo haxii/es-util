e4e8c3a8c0bf0e76cfe912018af7af4a56c35baf
Zichao LI <zichao@haxii.com>
2022-01-13 22:41:03 +0800

    refactoring with opts, added refresh

--- util.go
+++ util.go
@@ -24,14 +24,28 @@ func Int64ArrToInterfaceArray(a []int64) []interface{} {
 	return b
 }
 
+type BulkOpt struct {
+	Limit          int           // 每批数据量 默认 1000
+	Gap            time.Duration // 每批插入的时间间隔 默认 1s
+	Refresh        string        // 是否 refresh 默认 false
+	IgnoreConflict bool          // 是否忽略冲突 默认 false
+}
+
+type SingleReqMaker func(int) elastic.BulkableRequest
+
+// MakeRequest 分批执行 ES 的批量请求，避免 payload 过大造成集群宕机，每批 1k, 间隔 1s
+func MakeRequest(es *elastic.Client, bulkSize int, makeSingleRequest SingleReqMaker) (successCount int, err error) {
+	return MakeRequestWithOpt(es, bulkSize, BulkOpt{}, makeSingleRequest)
+}
+
 // MakeBulkESRequest 分批执行 ES 的批量请求，避免 payload 过大造成集群宕机
 //
 // bulkSize 最终插入的总数据量, limit 为每批批量插入的数据量, reqGap 为每批插入的时间间隔,
 // makeSingleRequest 应按数据的序号返回对应的每个批量请求的具体内容,
 // 返回成功的数量
-func MakeBulkESRequest(es *elastic.Client, bulkSize, limit int, reqGap time.Duration,
-	makeSingleRequest func(int) elastic.BulkableRequest) (int, error) {
-	return makeBulkESRequest(es, bulkSize, limit, false, reqGap, makeSingleRequest)
+func MakeBulkESRequest(es *elastic.Client, bulkSize, limit int,
+	reqGap time.Duration, makeSingleRequest SingleReqMaker) (int, error) {
+	return MakeRequestWithOpt(es, bulkSize, BulkOpt{Limit: limit, Gap: reqGap}, makeSingleRequest)
 }
 
 // MakeBulkESRequestIgnoreConflict 分批执行 ES 的批量请求，忽略版本冲突, 避免 payload 过大造成集群宕机
@@ -39,23 +53,37 @@ func MakeBulkESRequest(es *elastic.Client, bulkSize, limit int, reqGap time.Dura
 // bulkSize 最终插入的总数据量, limit 为每批批量插入的数据量, reqGap 为每批插入的时间间隔,
 // makeSingleRequest 应按数据的序号返回对应的每个批量请求的具体内容,
 // 返回成功的数量
-func MakeBulkESRequestIgnoreConflict(es *elastic.Client, bulkSize, limit int, reqGap time.Duration,
-	makeSingleRequest func(int) elastic.BulkableRequest) (int, error) {
-	return makeBulkESRequest(es, bulkSize, limit, true, reqGap, makeSingleRequest)
+func MakeBulkESRequestIgnoreConflict(es *elastic.Client, bulkSize, limit int,
+	reqGap time.Duration, makeSingleRequest SingleReqMaker) (int, error) {
+	return MakeRequestWithOpt(es, bulkSize, BulkOpt{Limit: limit, Gap: reqGap, IgnoreConflict: true}, makeSingleRequest)
 }
 
-func makeBulkESRequest(es *elastic.Client, bulkSize, limit int, ignoreConflict bool,
-	reqGap time.Duration, makeSingleRequest func(int) elastic.BulkableRequest) (successCount int, err error) {
+func MakeRequestWithOpt(es *elastic.Client, bulkSize int, opt BulkOpt,
+	makeSingleRequest SingleReqMaker) (successCount int, err error) {
 	// 保证传入数据的正确性
+	limit := opt.Limit
+	ignoreConflict := opt.IgnoreConflict
+	refresh := opt.Refresh
+	reqGap := opt.Gap
 	if es == nil || makeSingleRequest == nil {
 		return 0, errors.New("invalid es client or index")
 	}
 	if bulkSize <= 0 {
 		return 0, nil
 	}
-	if limit <= 0 || limit > bulkSize {
+	if limit <= 0 {
+		limit = 1000
+	}
+	if limit > bulkSize {
 		limit = bulkSize
 	}
+	if len(refresh) == 0 {
+		refresh = "false"
+	}
+	if reqGap <= 0 {
+		reqGap = time.Second
+	}
+
 	// 分割次数
 	round := bulkSize/limit + 1
 	shouldSleep := false
@@ -80,7 +108,7 @@ func makeBulkESRequest(es *elastic.Client, bulkSize, limit int, ignoreConflict b
 		if shouldSleep { // 非第一次批量请求前执行 sleep
 			time.Sleep(reqGap)
 		}
-		resp, bulkReqErr := bulkRequest.Do(context.TODO())
+		resp, bulkReqErr := bulkRequest.Refresh(refresh).Do(context.TODO())
 		shouldSleep = true
 		if bulkReqErr != nil {
 			return 0, bulkReqErr
